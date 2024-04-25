d258b64054327000755a66fcbd8729a0ce58b2e8
prayagyadav <prayagyadav206@gmail.com>
2024-04-25 18:37:25 +0530

    Automated Commit

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+      <link
+          rel='stylesheet'
+          href='https://cdn.jsdelivr.net/npm/katex@0.15.3/dist/katex.min.css'
+          integrity='sha384-KiWOvVjnN8qwAZbuQyWDIbfCLFhLXNETzBQjA/92pIowpC0d2O3nppDGQVgwd2nB'
+          crossorigin='anonymous'
+        />
         <meta name="robots" content="follow, index" />
         <meta name="description" content={meta.description} />
         <meta property="og:site_name" content={meta.title} />
